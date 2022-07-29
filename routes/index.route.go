b2cd6cb359cb22f26493e5fc5daae1c4a9aa2958
ghalmasshanditya <ghalmasshandityaaa@gmail.com>
2022-07-29 11:16:45 +0700

    feat: add static route

--- routes/index.route.go
+++ routes/index.route.go
@@ -3,10 +3,14 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"go-fiber-api/config"
 	"go-fiber-api/handlers"
 )
 
 func RouteInitialization(route *fiber.App) {
+	/** Static route */
+	route.Static("/public", config.ProjectRootPath+"./public/assets")
+
 	/** versioning api */
 	api := route.Group("/api/v1", logger.New())
 
